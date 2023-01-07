b910757314384ee4b4953ec33e88bad7dc4bdd2a
ryoughi <3310161374@qq.com>
2023-01-07 15:32:56 +0800

    ✨

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,38 @@
 <template>
-  <div>我是首页</div>
+  <!-- 1.组件默认吧template里面的根元素作为挂载点 -->
+  <!-- 一个template 只能有一个根元素
+       因为vue默认挂载点只能一个 -->
+  <!-- <div>我是首页</div> -->
+  <div class="box">
+    <h1>我是根组件</h1>
+    <!-- 使用组件:当成自定义html标签 -->
+    <MyGoods></MyGoods>
+  </div>
 </template>
 
 <script>
+//  导入组件
+import MyGoods from "@/MyGoods.vue"
+// 2.组件 js代码
+// 组件vue实例
 export default {
-
+  // 为啥组价data必须是一个函数
+  /* 因为组件要复用，保证每一次复用数据是独立的 */
+  /* 因为组件是需要复用的，如果组件data是一个对象，那么组件在复用的时候就会使用相同的对象地址。一旦在一个地方被修改，其
+他的也会跟着修改。如果组件data是一个函数，组件在每一次复用的时候就会调用这个函数得到一个全新的对象，这样就可以做到在复用时候每个组件之间的数据都是独立的的.互不影响.
+复用的时候海个组件之间的数据都是独立的，互不影响。 */
+// 注册组件
+components:{ MyGoods },
+data() {
+  return{
+    name:'ikun'
+  }
+}
 }
 </script>
 
 <style>
-
+.box{
+  background-color: #f00;
+}
 </style>
---
+++ src/MyGoods.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="box">
+    <p>商品名称:</p>
+    <p>商品价格:</p>
+    <p>商品编号:</p>
+  </div>
+</template>
+
+<script>
+export default {
+               
+}
+</script>
+
+<style>
+.box{
+    border: 2px solid #1f1177;
+}
+
+</style>
